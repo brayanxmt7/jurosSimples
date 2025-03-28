fa0d6e794e5ad84225809df20d218d72fd60fc6a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:14:33 -0300

    resolvid

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double capital, juros, tempo, total;
+
+Console.WriteLine("Capital [c] (R$).......:");
+capital = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Taxa de juros [i] (%)..:");
+juros = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Tempo [t] (meses)......:");
+tempo = Convert.ToDouble(Console.ReadLine());
+
+juros = capital * (juros / 100) * tempo;
+
+total = capital + juros;
+
+Console.WriteLine($"Montante (R$)..: {total}");
